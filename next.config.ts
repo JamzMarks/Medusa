e2d7bce555b1997ff8f147c1cfc0ad448d432816
JamzMarks <jamzmarks@gmail.com>
2025-04-07 15:32:59 -0300

    update next

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com', "fastly.picsum.photos"],
   },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
